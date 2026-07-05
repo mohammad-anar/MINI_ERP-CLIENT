9993558ac2206435e5931b458b646a1ed88ac2a5
mohammad-anar <ai.anarul4232@gmail.com>
2026-07-06 04:57:52 +0600

    base url updated

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import type { RootState } from '../store';
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api/v1',
+    baseUrl: 'https://mini-erp-server-34md.onrender.com/api/v1',
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.token;
       if (token) {
